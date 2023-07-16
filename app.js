86aba858dcdb951a912383aacacee1ac53374668
Milind <milind1995.mp@gmail.com>
2023-07-16 22:21:07 +0530

    Controller Updated

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express=require('express');
 const app=express();
-const createPerson=require('./Controller/createPerson')
+const {createPerson}=require('./Controller/personController')
 app.use(express.json())
 
 app.post('/person', createPerson);
+app.get('/list',list)
 
 module.exports=app;
